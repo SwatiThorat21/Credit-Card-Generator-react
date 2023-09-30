6594b62c4687da77c981cc89dba1a6a78e1087ee
Swati <swatic019@gmail.com>
2023-09-30 18:27:37 +0530

    frontCard css updated

--- src/components/CardFrontData.js
+++ src/components/CardFrontData.js
@@ -1,5 +1,19 @@
 import "../App.css";
 
-export default function CardFrontData(){
-
+export default function CardFrontData() {
+  return (
+    <>
+      <div className="card_fornt_wrapper">
+        <div className="circle_wrapper">
+          <div className="filled_circle"></div>
+          <div className="ring_circle"></div>
+        </div>
+        <div className="cardNumber">0000 0000 0000 0000</div>
+        <div className="nameCvc_wrapper">
+            <p>JANE APPLESEED</p>
+            <p>00/00</p>
+        </div>
+      </div>
+    </>
+  );
 }
