e46bec23a2fe5448b8ff1ef37ea8332f55c6b88e
Swati <swatic019@gmail.com>
2023-10-02 16:02:13 +0530

    css updated

--- src/components/CardBackData.js
+++ src/components/CardBackData.js
@@ -1,5 +1,32 @@
 import "../App.css";
 
-export default function CardFrontData(){
-
+export default function CardFrontData() {
+  return (
+    <>
+      <div className="card_back_wrapper">
+        <div className="black_scanner"></div>
+        <div className="cvc_input">000</div>
+        <div className="lines_wrapper">
+          <div className="line_contain">
+            <div className="line1-1"></div>
+            <div className="line1-2"></div>
+            <div className="line1-3"></div>
+            <div className="line1-4"></div>
+          </div>
+          <div className="line_contain">
+            <div className="line2-1"></div>
+            <div className="line2-2"></div>
+            <div className="line2-3"></div>
+            <div className="line2-4"></div>
+          </div>
+          <div className="line_contain">
+            <div className="line3-1"></div>
+            <div className="line3-2"></div>
+            <div className="line3-3"></div>
+            <div className="line3-4"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
